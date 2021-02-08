673ab24f7a9e3653c91e3101561e921f59aca9d3
michelle <njerimwanjiku@gmail.com>
2021-02-08 16:53:27 +0300

    callback and lamba functions

---
+++ fop.js
@@ -0,0 +1,34 @@
+const prepareTea = () => 'greenTea';
+
+const getTea = (numOfCups) => {
+  const teaCups = [];
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+const tea4TeamFCC = getTea(40)
+
+
+const prepareGreenTea = () => 'greenTea';
+const prepareBlackTea = () => 'blackTea';
+
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27)
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
